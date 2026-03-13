f5dfb7e9925b09f4b34ca0c910119e820c1b82e7
MarcosGG211 <marcos.garcia1986@alumnos.udg.mx>
2026-03-13 16:17:50 -0600

    Update Batalla Naval.c

--- Primer Trabajo en Equipo/Batalla Naval.c
+++ Primer Trabajo en Equipo/Batalla Naval.c
@@ -1,3 +1,5 @@
+Version 1
+
 #include <stdio.h>
 
 int main() {
@@ -60,3 +62,89 @@ int main() {
 
 hola 
 
+______________________________________________________________________________________________________________________________________________________
+
+Version 2
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int tablero[5][5];
+    int fila, columna;
+
+    // Nombre del jugador
+    char jugador[20];
+    printf("Nombre: ");
+    fgets(jugador, sizeof(jugador), stdin);
+
+    // Nombres de barcos
+    char barcos[3][15] = {"Fragata", "Destructor", "Submarino"};
+
+    // Inicializar tablero
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            tablero[i][j] = 0;
+        }
+    }
+
+    // Colocar barcos manualmente
+    tablero[0][1] = 1;
+    tablero[2][3] = 1;
+    tablero[4][4] = 1;
+
+    // Mostrar tablero inicial
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            printf("~ ");
+        }
+        printf("\n");
+    }
+
+    int barcosEncontrados = 0;
+
+    // Repetir durante varios turnos
+    for(int turno = 0; turno < 5; turno++) {
+
+        // Mostrar nombre en cada turno
+        printf("\nTurno de %s\n", jugador);
+        printf("Fila (1-5): ");
+        scanf("%d", &fila);
+        printf("Columna (1-5): ");
+        scanf("%d", &columna);
+
+        // Convertir entrada del usuario (1-5) a índice interno (0-4)
+        int f = fila - 1;
+        int c = columna - 1;
+
+        // Evaluar si el disparo fue impacto o agua
+        if(tablero[f][c] == 1) {
+            tablero[f][c] = 3;
+            printf("%s impacto un %s\n", jugador, barcos[barcosEncontrados]);
+            barcosEncontrados++;
+        } else {
+            printf("Agua...\n");
+            tablero[f][c] = 2;
+        }
+
+        // Mostrar nuevamente el tablero
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(tablero[i][j] == 0)      printf("~ ");
+                else if(tablero[i][j] == 1) printf("~ ");
+                else if(tablero[i][j] == 2) printf("O ");
+                else if(tablero[i][j] == 3) printf("X ");
+            }
+            printf("\n");
+        }
+    }
+
+    // Mensaje final
+    if(barcosEncontrados == 3) {
+        printf("\n%sgano la partida\n", jugador);
+    } else {
+        printf("\n%sno logró hundir todos los barcos\n", jugador);
+    }
+
+    return 0;
+}
